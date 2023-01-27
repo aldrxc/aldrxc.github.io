2e0cb040eeb097f41a5f24a1a4288a998e14d259
aldrxc <75245310+aldrxc@users.noreply.github.com>
2023-01-27 22:10:21 +0800

    updated 404 page styling and global.js loading of header and footer

--- js/global.js
+++ js/global.js
@@ -1,11 +1,4 @@
 $(document).ready(function () {
-    var url = window.location.href;
-    if(url.includes("https://aldrxc.github.io/")) {
-        $("#header").load("https://aldrxc.github.io/header.html");
-        $("#footer").load("https://aldrxc.github.io/footer.html");
-    }
-    else {
-        $("#header").load("./header.html");
-        $("#footer").load("./footer.html");
-    }
+    $("#header").load("https://aldrxc.github.io/header.html");
+    $("#footer").load("https://aldrxc.github.io/footer.html");
 });
