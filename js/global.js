db4030373646b64e6613d41a29b3a6e02ead18cb
aldrxc <75245310+aldrxc@users.noreply.github.com>
2023-01-27 21:57:58 +0800

    updated portfolio website project

--- js/global.js
+++ js/global.js
@@ -1,4 +1,11 @@
-$(function () {
-    $("#header").load("https://aldrxc.github.io/header.html");
-    $("#footer").load("https://aldrxc.github.io/footer.html");
+$(document).ready(function () {
+    var url = window.location.href;
+    if(url.includes("https://aldrxc.github.io/")) {
+        $("#header").load("https://aldrxc.github.io/header.html");
+        $("#footer").load("https://aldrxc.github.io/footer.html");
+    }
+    else {
+        $("#header").load("./header.html");
+        $("#footer").load("./footer.html");
+    }
 });
